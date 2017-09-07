92f8f0e65257cd274192f2636417f5ff7a0801f5
Nick Marasco <nicholas.marasco@ibm.com>
2017-09-07 09:07:47 -0400

    I don't know what I'm doing anymore

--- commandHandlers.js
+++ commandHandlers.js
@@ -346,7 +346,7 @@ exports.command_handler.fortuneHandler = function fortuneHandler(){
 }
 
 //handle timer command
-exports.command_handler.timerHandler = async function timerHandler(time){
+exports.command_handler.timerHandler = function timerHandler(time){
   var result = {};
   var msg = _resolveAfterX("Hopefully " + time + " seconds have passed", time);
   result["message"] = await msg;
