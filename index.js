b41cefba84533478563089ec6ebd24b43ee2614b
Nick Marasco <nicholas.marasco@ibm.com>
2017-10-05 16:25:21 -0400

    I might just be an idiot

--- index.js
+++ index.js
@@ -71,14 +71,14 @@ rtm.on(RTM_EVENTS.REACTION_ADDED, function handleReactionAdded(evnt) {
     file         : evnt.item.file,
     file_comment : evnt.item.file_comment,
     channel      : evnt.item.channel,
-    ts           : evnt.item.ts};
+    ts           : evnt.item.timestamp};
 
   //console.log("channel: " + item.channel + "\nts: " + item.ts);
   //console.log(item);
 
   web.reactions.add(evnt.reaction,item, function handleAdd(err,res){
     if(err){ console.log("channel: " + item.channel + "\n" +
-                         "ts     : " + item.ts + "\n\n" + err);}
+                         "ts     : " + item.timestamp + "\n\n" + err);}
     else{ console.log("Posted successfully"); }
   });
 });
