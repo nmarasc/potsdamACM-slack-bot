c2fc8254616e4e39e0e309208a5660d8df4feb46
Nicholas Marasco <nicdmarasco@gmail.com>
2017-10-07 17:58:30 -0400

    bring it down

--- bender.js
+++ bender.js
@@ -29,6 +29,8 @@ function Bender(rtm, web, opts){
  * Bender's welcome handler
  * @param {String} id - user id to welcome
  */
+
+asfasf
 Bender.prototype.welcome = function welcome(id){
   var bot_msg = "Welcome to our Slack! :congaparrot:\n" +
                 "Type <@" + this.bot_id + "> COMMANDS to get a list of bot commands!\n" +
