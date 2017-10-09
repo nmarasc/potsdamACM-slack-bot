8d7a6294b4b8f460d59d71ea4ea9bb3362c5f483
Nick Marasco <nicholas.marasco@ibm.com>
2017-10-09 13:08:45 -0400

    Things are weird again

--- handlers/pickitHandler.js
+++ handlers/pickitHandler.js
@@ -2,6 +2,7 @@ var util = require('../util.js').util;
 
 module.exports = function pickitHandler(ops){
   var result = {};
+  console.log(ops);
   var roll = util.doRoll(ops.length,1)[0];
   result["message"] = "I choose: " + ops[roll-1];
   return result;
